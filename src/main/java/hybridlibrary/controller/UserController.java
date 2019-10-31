f6016a0a87d3328387728a7497845925c512722a
Branko Paskali <paskalibane@gmail.com>
2019-10-31 14:17:44 +0100

    Added swagger.

--- src/main/java/hybridlibrary/controller/UserController.java
+++ src/main/java/hybridlibrary/controller/UserController.java
@@ -2,6 +2,10 @@ package hybridlibrary.controller;
 
 import hybridlibrary.dto.UserDTO;
 import hybridlibrary.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +22,19 @@ public class UserController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "View a list of available users", response = List.class)
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAll() {
         return ResponseEntity.ok(userService.findAll());
     }
 
+    @ApiOperation(value = "View a user with id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.findOne(id));

df2573cc7ff4df5deafc4a5e0a3f94bc19cf9ddd
Branko Paskali <paskalibane@gmail.com>
2019-10-31 22:41:24 +0100

    Switched to java 13.

--- src/main/java/hybridlibrary/controller/UserController.java
+++ src/main/java/hybridlibrary/controller/UserController.java
@@ -30,7 +30,7 @@ public class UserController {
 
     @ApiOperation(value = "View a user with id")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 200, message = "Successfully retrieved user"),
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")

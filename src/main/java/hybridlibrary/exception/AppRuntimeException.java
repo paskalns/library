15d27e012bd1c5c5fd3604af54635a2204b746d6
Branko Paskali <paskalibane@gmail.com>
2019-10-31 11:20:28 +0100

    Added log.error

--- src/main/java/hybridlibrary/exception/AppRuntimeException.java
+++
@@ -1,9 +0,0 @@
-package hybridlibrary.exception;
-
-public class AppRuntimeException extends RuntimeException {
-
-    public AppRuntimeException(String message) {
-        super(message);
-    }
-
-}

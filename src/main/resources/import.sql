cb3a630a15f3b2ebb90390da696cbc5b9f206807
Branko Paskali <paskalibane@gmail.com>
2019-11-08 15:16:52 +0100

    Integration and unit test added.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,7 @@ INSERT INTO user VALUES (1, 'Branko', 'Paskali', '$2a$10$75KqsZ9vyBsoBgTwEXjVUOF
 INSERT INTO user VALUES (2, 'Vladan', 'Bjelic', '$2a$10$75KqsZ9vyBsoBgTwEXjVUOF.ODJlE09UHHIw8H/yHJHxZumieonkm', 'ROLE_ADMIN', 'vbjelic');
 INSERT INTO user VALUES (3, 'Miodrag', 'Brkljac', '$2a$10$75KqsZ9vyBsoBgTwEXjVUOF.ODJlE09UHHIw8H/yHJHxZumieonkm', 'ROLE_USER', 'mbrkljac');
 
+INSERT INTO book VALUES (1, 'author', 20, 20, 'name', 123);
+INSERT INTO book VALUES (2, 'author', 20, 20, 'name', 223);
+INSERT INTO book VALUES (3, 'author', 20, 20, 'name', 222);
+
---
+++ src/test/java/hybridlibrary/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,58 @@
+package hybridlibrary.controller;
+
+import hybridlibrary.dto.UserDTO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.LinkedHashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerIntegrationTest {
+
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void findAll() {
+        ResponseEntity<UserDTO[]> responseEntity = restTemplate.withBasicAuth("bpaskali", "password")
+                .getForEntity("/users", UserDTO[].class);
+
+        UserDTO[] userDTOS = responseEntity.getBody();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void findAllWithoutPermission() {
+        ResponseEntity<LinkedHashMap> responseEntity = restTemplate.withBasicAuth("mbrkljac", "password")
+                .getForEntity("/users", LinkedHashMap.class);
+
+        LinkedHashMap linkedHashMap = responseEntity.getBody();
+
+        assertEquals(HttpStatus.FORBIDDEN, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void findById() {
+        ResponseEntity<UserDTO> responseEntity = restTemplate.withBasicAuth("bpaskali", "password")
+                .getForEntity("/users/2", UserDTO.class);
+
+        UserDTO userDTO = responseEntity.getBody();
+
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Autowired
+    public void setRestTemplate(TestRestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+}
